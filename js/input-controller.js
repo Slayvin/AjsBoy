1dae7edcbf65d319e8fc95978937d852b6a6214b
Slayvin <sylvain.hittelet@gmail.com>
2018-05-20 00:28:05 +0200

    Updated input events + arrow graphics

--- js/input-controller.js
+++ js/input-controller.js
@@ -2,12 +2,12 @@
 var keyMap = {
 	'j': 'A',
 	'i': 'B',
-	'z': 'Up',
-	's': 'Down',
-	'q': 'Left',
-	'd': 'Right',
-	' ': 'Select',
-	'Enter': 'Start'
+	'z': 'up',
+	's': 'down',
+	'q': 'left',
+	'd': 'right',
+	' ': 'select',
+	'Enter': 'start'
 };
 
 document.addEventListener('keydown', (event) => {
@@ -15,8 +15,9 @@ document.addEventListener('keydown', (event) => {
 	Object.keys(keyMap).forEach((key) => {
 		if (key === keyName) {
 //			window.console.log(keyMap[key]+' Down');
-	// Add interrupt
-	// Update memory input register
+			document.getElementById('key-' + keyMap[key]).classList.add('pressed');
+			// Add interrupt
+			// Update memory input register
 		}
 	});
 });
@@ -25,7 +26,8 @@ document.addEventListener('keyup', (event) => {
 	Object.keys(keyMap).forEach((key) => {
 		if (key === keyName) {
 //			window.console.log(keyMap[key]+' Up');
-	// Update memory input register
+			document.getElementById('key-' + keyMap[key]).classList.remove('pressed');
+			// Update memory input register
 		}
 	});
 	event.preventDefault();
