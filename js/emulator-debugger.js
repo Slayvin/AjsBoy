854b17b1aec4bc54fc3ec37960682f8684d3609e
Sylvain <sylvain.hittelet@softedge.be>
2026-03-14 20:42:40 +0100

    Updated debugger
    converted to class
    removed memory map image

--- js/emulator-debugger.js
+++ js/emulator-debugger.js
@@ -1,155 +1,153 @@
-/* global gbEmu */
 /* global Lcd */
 'use strict';
 
-/**
- * 
- * @param {gbEmu} emulator
- * @returns {gbEmu.debugger}
- */
-gbEmu.debugger = function (emulator) {
-	this.states = {
-		'debug-cpu': true,
-		'debug-cpu-registers': true,
-		'debug-cpu-flags': true,
-		'debug-call-stack': true,
-		'debug-io-registers': true,
-		'debug-lcd-status': true,
-		'debug-memory-map': true,
-		'debug-bg-palette': true,
-		'debug-sprite0-palette': true,
-		'debug-sprite1-palette': true,
-		'debug-vram-map': true,
-		'debug-bg-map': true
-	};
-	this.fps = document.getElementById('fps');
-	this.previousTime = 0;
-	this.minFps = 1000;
-	this.maxFps = 0;
+gbEmu.debugger = class {
+	/**
+	 * 
+	 * @param {gbEmu} emulator
+	 * @returns {gbEmu.debugger}
+	 */
+	constructor(emulator) {
+		this.states = {
+			'debug-cpu': true,
+			'debug-cpu-registers': true,
+			'debug-cpu-flags': true,
+			'debug-call-stack': true,
+			'debug-io-registers': true,
+			'debug-lcd-status': true,
+			'debug-bg-palette': true,
+			'debug-sprite0-palette': true,
+			'debug-sprite1-palette': true,
+			'debug-vram-map': true,
+			'debug-bg-map': true
+		};
 
-	this.cpu = emulator.cpu;
-	this.mmu = emulator.mmu;
-	this.lcd = emulator.lcd;
+		this.previousTime = 0;
+		this.minFps = 1000;
+		this.maxFps = 0;
 
-	this.pc = document.querySelector('#pc');
-	this.code = document.querySelector('#code');
-	this.sp = document.querySelector('#sp');
-	this.stack = document.querySelectorAll('#stack .cell');
-	const cpuRegisters = ['A', 'B', 'C', 'D', 'E', 'F', 'H', 'L'];
-	this.getCpuRegisters = function () {
-		return cpuRegisters;
-	};
+		this.cpu = emulator.cpu;
+		this.mmu = emulator.mmu;
+		this.lcd = emulator.lcd;
 
-	cpuRegisters.forEach((r) => {
-		this['reg' + r] = document.querySelector('#reg-' + r);
-	});
-	const flags = ['Z', 'N', 'H', 'C'];
-	flags.forEach((f) => {
-		this['flag' + f] = document.querySelector('#flag-' + f);
-	});
-	this.ly = document.querySelector('#lcd-ly');
-	this.lcdc = document.querySelector('#lcd-c');
-	const ioRegisters = ['FF00', 'FF01', 'FF02', 'FF04', 'FF05', 'FF06', 'FF07', 'FF0F',
-		'FF40', 'FF41', 'FF42', 'FF43', 'FFFF'];
-	ioRegisters.forEach((ioAddress) => {
-		this[ioAddress] = document.getElementById('io-' + ioAddress);
-	});
-	this.bgMap = document.querySelector('#lcd-background canvas').getContext("2d");
-	this.tileMap = document.querySelector('#tile-map canvas').getContext("2d");
-	this.bgPalette = document.querySelector('#palette-bg canvas').getContext("2d");
-	this.objPalette0 = document.querySelector('#palette-0 canvas').getContext("2d");
-	this.objPalette1 = document.querySelector('#palette-1 canvas').getContext("2d");
-	this.memoryMap = document.querySelector('#memory canvas').getContext("2d");
-	this.lcdBuffer = document.querySelector('#lcd canvas').getContext("2d");
+		this.debuggerUi = {
+			fps: document.getElementById('fps'),
+			pc: document.querySelector('#pc'),
+			sp: document.querySelector('#sp'),
+			code: document.querySelector('#code'),
+			stack: document.querySelectorAll('#stack .cell'),
+			ly: document.querySelector('#lcd-ly'),
+			lcdc: document.querySelector('#lcd-c')
+		};
 
-	//Off-screen canvas
-	this.tmpCanvas = document.createElement('canvas');
-	this.tmpCanvas.width = 256;
-	this.tmpCanvas.height = 256;
-	this.background = this.tmpCanvas.getContext('2d');
-	this.background.lineWidth = 1;
-	this.background.strokeStyle = '#55aaff';
-};
+		const cpuRegisters = ['A', 'B', 'C', 'D', 'E', 'F', 'H', 'L'];
+		this.getCpuRegisters = () => cpuRegisters;
 
-gbEmu.debugger.prototype.updateFPS = function (timestamp) {
-	var fps = 1000 / (timestamp - this.previousTime);
-//	if (fps < this.minFps && timestamp>1000) {
-//		this.minFps = fps;
-//	}
-//	if (fps > this.maxFps) {
-//		this.maxFps = fps;
-//	}
-	this.fps.innerHTML = fps.toFixed(2);// + ' (min:' + this.minFps.toFixed(2) + ' - max:' + this.maxFps.toFixed(2) + ')';
-	this.previousTime = timestamp;
-};
 
-gbEmu.debugger.prototype.update = function () {
-
-	if (this.states['debug-cpu']) {
-		this.pc.innerHTML = this.cpu.PC.toString(16);
-		this.sp.innerHTML = this.cpu.SP.toString(16);
-		this.code.innerHTML = this.mmu.memory[this.cpu.PC].toString(16);
-	}
-
-	if (this.states['debug-call-stack']) {
-		for (var s = 0; s < 16; s++) {
-			this.stack[s].innerHTML = this.mmu.read16(0xfffe - s * 2).toString(16).toUpperCase();
-			// Note: the stack doesn't necessarily starts at 0xFFFE ! A program could set the initial pointer to another address.
-			// This means the value displayed from 0xFFFE and below doesn't always represent the stack!
-			// TODO : read stack values whenever PUSH or POP functions are triggered, or set correct address when LD SP function is triggered
-		}
-	}
-
-	if (this.states['debug-cpu-registers']) {
-		this.getCpuRegisters().forEach((r) => {
-			this['reg' + r].innerHTML = this.cpu[r].toString(16).toUpperCase();
+		cpuRegisters.forEach((r) => {
+			this.debuggerUi['reg' + r] = document.querySelector('#reg-' + r);
 		});
-	}
-
-	if (this.states['debug-cpu-flags']) {
-		this.flagZ.innerHTML = this.cpu.flags.Z.toString(2);
-		this.flagN.innerHTML = this.cpu.flags.N.toString(2);
-		this.flagH.innerHTML = this.cpu.flags.H.toString(2);
-		this.flagC.innerHTML = this.cpu.flags.C.toString(2);
-	}
-
-	if (this.states['debug-lcd-status']) {
-		this.ly.innerHTML = this.mmu.read8(0xff44);
-		this.lcdc.innerHTML = this.mmu.read8(0xff40).toString(16);
-	}
-
-	if (this.states['debug-io-registers']) {
-		const ioRegisters = {FF00: 0xff00, FF01: 0xff01, FF02: 0xff02, FF04: 0xff04, FF05: 0xff05, FF06: 0xff06, FF07: 0xff07, FF0F: 0xff0f,
-			FF40: 0xff40, FF41: 0xff41, FF42: 0xff42, FF43: 0xff43, FFFF: 0xffff};
-		Object.keys(ioRegisters).forEach((ioAddress) => {
-			this[ioAddress].innerHTML = this.mmu.read8(ioRegisters[ioAddress]).toString(16).toUpperCase();
+		const flags = ['Z', 'N', 'H', 'C'];
+		flags.forEach((f) => {
+			this.debuggerUi['flag' + f] = document.querySelector('#flag-' + f);
 		});
-	}
-	if (this.states['debug-memory-map']) {
-		var iData = new ImageData(new Uint8ClampedArray(this.mmu.memory.buffer), 64, 256);
-		this.memoryMap.putImageData(iData, 0, 0);
-	}
-//	if (false) {
-//		var iData = new ImageData(this.lcd.data, 160, 144);
-//		this.lcdBuffer.putImageData(iData, 0, 0);
-//	}
 
-//	var iData = new ImageData(new Uint8ClampedArray(this.mmu.tileMap), 128, 192);
-//	this.tileMap.putImageData(iData, 0, 0);
+		const ioRegisters = ['FF00', 'FF01', 'FF02', 'FF04', 'FF05', 'FF06', 'FF07', 'FF0F',
+			'FF40', 'FF41', 'FF42', 'FF43', 'FFFF'];
+		ioRegisters.forEach((ioAddress) => {
+			this.debuggerUi[ioAddress] = document.getElementById('io-' + ioAddress);
+		});
 
-	if (this.states['debug-vram-map']) {
-		for (var tile = 0; tile < 384; tile++) {
-			var tileData = this.getTileData(tile);
-			this.tileMap.putImageData(tileData, 8 * (tile % 16), 8 * Math.floor(tile / 16));
+		this.bgMap = document.querySelector('#lcd-background canvas').getContext("2d");
+		this.tileMap = document.querySelector('#tile-map canvas').getContext("2d");
+		this.bgPalette = document.querySelector('#palette-bg canvas').getContext("2d");
+		this.objPalette0 = document.querySelector('#palette-0 canvas').getContext("2d");
+		this.objPalette1 = document.querySelector('#palette-1 canvas').getContext("2d");
+		this.lcdBuffer = document.querySelector('#lcd canvas').getContext("2d");
+
+		//Off-screen canvas
+		this.tmpCanvas = document.createElement('canvas');
+		this.tmpCanvas.width = 256;
+		this.tmpCanvas.height = 256;
+		this.background = this.tmpCanvas.getContext('2d');
+		this.background.lineWidth = 1;
+		this.background.strokeStyle = '#55aaff';
+	}
+
+	/**
+	 *
+	 * @param {number} timestamp
+	 */
+	updateFPS(timestamp) {
+		const fps = 1000 / (timestamp - this.previousTime);
+		//	if (fps < this.minFps && timestamp>1000) {
+		//		this.minFps = fps;
+		//	}
+		//	if (fps > this.maxFps) {
+		//		this.maxFps = fps;
+		//	}
+		this.debuggerUi.fps.innerHTML = fps.toFixed(2); // + ' (min:' + this.minFps.toFixed(2) + ' - max:' + this.maxFps.toFixed(2) + ')';
+		this.previousTime = timestamp;
+	}
+
+	update() {
+
+		if (this.states['debug-cpu']) {
+			this.debuggerUi.pc.innerHTML = this.cpu.PC.toString(16);
+			this.debuggerUi.sp.innerHTML = this.cpu.SP.toString(16);
+			this.debuggerUi.code.innerHTML = this.mmu.memory[this.cpu.PC].toString(16);
 		}
-	}
-	// Update background
-	if (this.states['debug-bg-map']) {
 
-		var ctx = this.background;
-		if (!false) {
-			var scrollY = this.mmu.read8(0xFF42);
-			var scrollX = this.mmu.read8(0xFF43);
+		if (this.states['debug-call-stack']) {
+			for (let s = 0; s < 16; s++) {
+				this.debuggerUi.stack[s].innerHTML = this.mmu.read16(0xfffe - s * 2).toString(16).toUpperCase();
+				// Note: the stack doesn't necessarily starts at 0xFFFE ! A program could set the initial pointer to another address.
+				// This means the value displayed from 0xFFFE and below doesn't always represent the stack!
+				// TODO : read stack values whenever PUSH or POP functions are triggered, or set correct address when LD SP function is triggered
+			}
+		}
+
+		if (this.states['debug-cpu-registers']) {
+			this.getCpuRegisters().forEach((r) => {
+				this.debuggerUi['reg' + r].innerHTML = this.cpu[r].toString(16).toUpperCase();
+			});
+		}
+
+		if (this.states['debug-cpu-flags']) {
+			this.debuggerUi.flagZ.innerHTML = this.cpu.flags.Z.toString(2);
+			this.debuggerUi.flagN.innerHTML = this.cpu.flags.N.toString(2);
+			this.debuggerUi.flagH.innerHTML = this.cpu.flags.H.toString(2);
+			this.debuggerUi.flagC.innerHTML = this.cpu.flags.C.toString(2);
+		}
+
+		if (this.states['debug-lcd-status']) {
+			this.debuggerUi.ly.innerHTML = this.mmu.read8(0xff44);
+			this.debuggerUi.lcdc.innerHTML = this.mmu.read8(0xff40).toString(16);
+		}
+
+		if (this.states['debug-io-registers']) {
+			const ioRegisters = {
+				FF00: 0xff00, FF01: 0xff01, FF02: 0xff02, FF04: 0xff04, FF05: 0xff05, FF06: 0xff06, FF07: 0xff07, FF0F: 0xff0f,
+				FF40: 0xff40, FF41: 0xff41, FF42: 0xff42, FF43: 0xff43, FFFF: 0xffff
+			};
+			Object.keys(ioRegisters).forEach((ioAddress) => {
+				this.debuggerUi[ioAddress].innerHTML = this.mmu.read8(ioRegisters[ioAddress]).toString(16).toUpperCase();
+			});
+		}
+
+		if (this.states['debug-vram-map']) {
+			for (let tile = 0; tile < 384; tile++) {
+				const tileData = this.getTileData(tile);
+				this.tileMap.putImageData(tileData, 8 * (tile % 16), 8 * Math.floor(tile / 16));
+			}
+		}
+
+		// Update background
+		if (this.states['debug-bg-map']) {
+			const ctx = this.background;
+
+			const scrollY = this.mmu.read8(0xFF42);
+			const scrollX = this.mmu.read8(0xFF43);
 
 			ctx.beginPath();
 			if (scrollX > (256 - 160)) {
@@ -158,75 +156,80 @@ gbEmu.debugger.prototype.update = function () {
 			if (scrollY > (256 - 144)) {
 				ctx.rect(scrollX, scrollY - 256, 160, 144);
 			}
-			if (scrollX > (256 - 160) && scrollY > (256 - 144)) {
+		 	if (scrollX > (256 - 160) && scrollY > (256 - 144)) {
 				ctx.rect(scrollX - 256, scrollY - 256, 160, 144);
 			}
 			ctx.rect(scrollX, scrollY, 160, 144);
+			
 			ctx.stroke();
 			ctx.closePath();
+
+			this.bgMap.drawImage(this.tmpCanvas, 0, 0);
 		}
-		this.bgMap.drawImage(this.tmpCanvas, 0, 0);
-	}
 
 
-	// Palettes
-	if (this.states['debug-bg-palette']) {
-		var iData = new ImageData(new Uint8ClampedArray(this.lcd.backgroundPalette.getImageData()), 4, 1);
-		this.bgPalette.putImageData(iData, 0, 0);
-	}
-	if (this.states['debug-sprite0-palette']) {
-		var iData = new ImageData(new Uint8ClampedArray(this.lcd.spritePalette0.getImageData()), 4, 1);
-		this.objPalette0.putImageData(iData, 0, 0);
-	}
-	if (this.states['debug-sprite1-palette']) {
-		var iData = new ImageData(new Uint8ClampedArray(this.lcd.spritePalette1.getImageData()), 4, 1);
-		this.objPalette1.putImageData(iData, 0, 0);
-	}
-};
-
-gbEmu.debugger.prototype.updateLcd = function () {
-	for (var addr = 0; addr < (32 * 32); addr++) {
-		var tile = this.mmu.vram.tileMap0[addr];
-//			if(tile<128){tile+=256;}// depends on LCDC ?
-		var tileData = this.getTileData(tile);
-		this.background.putImageData(tileData, 8 * (addr % 32), 8 * Math.floor(addr / 32));
-	}
-
-	var scrollY = this.mmu.read8(0xFF42);
-	var scrollX = this.mmu.read8(0xFF43);
-
-	this.lcdBuffer.drawImage(this.tmpCanvas, scrollX, scrollY, 160, 144, 0, 0, 160, 144);
-};
-gbEmu.debugger.prototype.updateTileMap = function () {
-	for (var addr = 0; addr < (6144); addr++) {// 64px * 96px
-		var lo = this.mmu.readVram(addr * 2);
-		var hi = this.mmu.readVram(addr * 2 + 1);
-		for (var i = 0; i < 8; i++) {
-			var colorId = this.lcd.getColorId(lo, hi, i);
-			var color = this.lcd.backgroundPalette.getColorRgba(colorId);
-			var address = (4 * i) + (32 * addr);
-			this.mmu.tileMap[address + 0] = color[0];
-			this.mmu.tileMap[address + 1] = color[1];
-			this.mmu.tileMap[address + 2] = color[2];
-			this.mmu.tileMap[address + 3] = 0xFF;
+		// Palettes
+		if (this.states['debug-bg-palette']) {
+			const iData = new ImageData(new Uint8ClampedArray(this.lcd.backgroundPalette.getImageData()), 4, 1);
+			this.bgPalette.putImageData(iData, 0, 0);
+		}
+		if (this.states['debug-sprite0-palette']) {
+			const iData = new ImageData(new Uint8ClampedArray(this.lcd.spritePalette0.getImageData()), 4, 1);
+			this.objPalette0.putImageData(iData, 0, 0);
+		}
+		if (this.states['debug-sprite1-palette']) {
+			const iData = new ImageData(new Uint8ClampedArray(this.lcd.spritePalette1.getImageData()), 4, 1);
+			this.objPalette1.putImageData(iData, 0, 0);
 		}
 	}
-};
 
-/**
- * 
- * @param {type} id
- * @returns {ImageData}
- */
-gbEmu.debugger.prototype.getTileData = function (id) {
-	var lcdc = this.mmu.read8(0xff40);
-	var tile = id;
-	if ((0x10 & lcdc) === 0) {
-		if (tile < 128) {
-			tile += 256;
+	updateLcd() {
+		for (let addr = 0; addr < (32 * 32); addr++) {
+			const tile = this.mmu.vram_tileMap0[addr];
+			const tileData = this.getTileData(tile);
+			this.background.putImageData(tileData, 8 * (addr % 32), 8 * Math.floor(addr / 32));
+		}
+
+		const scrollY = this.mmu.read8(0xFF42);
+		const scrollX = this.mmu.read8(0xFF43);
+
+		this.lcdBuffer.drawImage(this.tmpCanvas, scrollX, scrollY, 160, 144, 0, 0, 160, 144);
+	}
+
+	updateTileMap() {
+		for (let addr = 0; addr < (6144); addr++) { // 64px * 96px
+			const lo = this.mmu.readVram(addr * 2);
+			const hi = this.mmu.readVram(addr * 2 + 1);
+			for (let i = 0; i < 8; i++) {
+				const colorId = this.lcd.getColorId(lo, hi, i);
+				const color = this.lcd.backgroundPalette.getColorRgba(colorId);
+				const address = (4 * i) + (32 * addr);
+				this.mmu.tileMap[address + 0] = color[0];
+				this.mmu.tileMap[address + 1] = color[1];
+				this.mmu.tileMap[address + 2] = color[2];
+				this.mmu.tileMap[address + 3] = 0xFF;
+			}
 		}
 	}
-	var tileArray = new Uint8ClampedArray(this.mmu.tileMap.buffer, tile * 256, 256);
-	var tileData = new ImageData(tileArray, 8, 8);
-	return tileData;
-};
+
+	/**
+	 *
+	 * @param {number} id
+	 * @returns {ImageData}
+	 */
+	getTileData(id) {
+		const lcdc = this.mmu.read8(0xff40);
+		let tile = id;
+
+		// Reads tile data from 0x8000 or 0x8800 depending on bit 4 of LCDC register
+		if ((0x10 & lcdc) === 0) {
+			if (tile < 128) {
+				tile += 256;
+			}
+		}
+		const tileArray = new Uint8ClampedArray(this.mmu.tileMap.buffer, tile * 256, 256);
+		const tileData = new ImageData(tileArray, 8, 8);
+
+		return tileData;
+	}
+}
